0e7ec75adb62cdea6b0d259ff9995b43e47e263c
Konstantin Krivlenia <krivlenia@gmail.com>
2016-06-08 14:54:32 +0300

    fix isComponentRootNode method for rect 15.*

--- src/api-adapter/react/create-api.js
+++ src/api-adapter/react/create-api.js
@@ -135,7 +135,7 @@ function getAdditionalInstanceInfo(element) {
   }
 
   if (info && Array.isArray(info.decorators)) {
-    decorators = info.decorators.map(function(decorator, idx) {
+    decorators = info.decorators.map(function (decorator, idx) {
       var wrapperClass = wrapperClassMap[idx];
       var classLoc;
       var renderLoc;
@@ -230,14 +230,14 @@ function getNestedComponentNodeLocation(node) {
  * @param config {{reactApi: reactApi, ClassView: ClassView, InstanceView: InstanceView}}
  * @returns {{isComponentRootNode: isComponentRootNode, getComponentNameByNode: getComponentNameByNode, getInstanceByNode: getReactElementByNode, getInstanceRootNode: getInstanceRootNode, getInstanceClass: getInstanceClass, getInstanceLocation: getInstanceLocation, getInstanceRenderLocation: getInstanceRenderLocation, getNodeLocation: getNodeLocation, getAdditionalInstanceInfo: getAdditionalInstanceInfo, showDefaultInfo: showDefaultInfo, getNestedComponentNameByNode: getComponentNameByNode, getNestedComponentNodeLocation: getNestedComponentNodeLocation, viewAttributeFilter: viewAttributeFilter}}
  */
-module.exports = function(config) {
+module.exports = function (config) {
   var reactApi = config.reactApi;
 
   createClassView = config.createClassView;
   createInstanceView = config.createInstanceView;
 
   if (reactApi.ComponentTree) {
-    getReactElementByNode = function(node) {
+    getReactElementByNode = function (node) {
       if (!node) {
         return null;
       }
@@ -245,9 +245,17 @@ module.exports = function(config) {
       return element && element._currentElement != null ? element._currentElement._owner : null;
     };
 
-    getInstanceRootNode = function(element) {
+    getInstanceRootNode = function (element) {
       return reactApi.ComponentTree.getNodeFromInstance(element);
     };
+
+    isComponentRootNode = function (node) {
+      var element = getReactElementByNode(node);
+      if (!element) {
+        return false;
+      }
+      return getInstanceRootNode(element) === node;
+    }
   } else if (reactApi.Mount.getID && reactApi.Mount.getNode) {
     getID = reactApi.Mount.getID;
     getNode = reactApi.Mount.getNode;
@@ -255,12 +263,12 @@ module.exports = function(config) {
     // patch React
     var _mount = reactApi.Reconciler.mountComponent;
     var _unmount = reactApi.Reconciler.unmountComponent;
-    reactApi.Reconciler.mountComponent = function(instance) {
+    reactApi.Reconciler.mountComponent = function (instance) {
       var res = _mount.apply(this, arguments);
       elementMap[instance._rootNodeID] = instance;
       return res;
     };
-    reactApi.Reconciler.unmountComponent = function(instance) {
+    reactApi.Reconciler.unmountComponent = function (instance) {
       delete elementMap[instance._rootNodeID];
       return _unmount.apply(this, arguments);
     };
@@ -278,13 +286,13 @@ module.exports = function(config) {
     getInstanceRenderLocation: getInstanceRenderLocation,
     getNodeLocation: getNodeLocation,
     getAdditionalInstanceInfo: getAdditionalInstanceInfo,
-    showDefaultInfo: function() {
+    showDefaultInfo: function () {
       return false;
     },
 
     getNestedComponentNameByNode: getComponentNameByNode,
     getNestedComponentNodeLocation: getNestedComponentNodeLocation,
-    viewAttributeFilter: function(attr) {
+    viewAttributeFilter: function (attr) {
       return attr.name !== 'data-reactid';
     }
   };
